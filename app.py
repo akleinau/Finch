8f8a4316e607f64699dfb64992ab0a5bfa422698
akleinau <56925445+akleinau@users.noreply.github.com>
2024-07-24 13:22:59 +0200

    add logo
    Former-commit-id: 8299cb9da3f5947718c7a3e0182e653f13567f21

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ ds = DataStore.DataStore()
 
 template = pn.template.MaterialTemplate(
     title="FINCH - Feature Interaction Charts",
+    logo="https://github.com/akleinau/Finch/blob/d7be6ce2306b34ef40dd572ed079b7e1b43047a3/logo.png?raw=true",
     header_background =color,
     sidebar_width=300,
 )
